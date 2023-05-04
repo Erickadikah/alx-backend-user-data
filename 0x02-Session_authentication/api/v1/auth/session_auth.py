e279dfd77843c338aef2cc5fa1ca93d05be9f8de
erickadikah <erickadikah2030@gmail.com>
2023-05-05 01:49:22 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,11 +2,13 @@
 """Session Class"""
 import os
 from api.v1.auth.auth import Auth
+from models.user_session import UserSession
 
-
+if os.getenv('AUTH_TYPE') == 'session':
+    User = UserSession
+else:
+    User = None
 class SessionAuth(Auth):
     """Class SessionAuth
     """
-    def __init__(self):
-        """initilaizer"""
-        super().__init__()
+    pass
