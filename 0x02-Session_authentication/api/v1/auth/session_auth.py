03986fd4cb60df755a54af159df59c85c983aec9
erickadikah <erickadikah2030@gmail.com>
2023-05-05 01:41:08 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Session Class"""
-
+import os
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Class SessionAuth
     """
-    pass
+    def __init__(self):
+        """initilaizer"""
+        super().__init__()
