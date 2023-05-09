16183252bc62c6c2ab62b5b16d5292ac1fb961aa
erickadikah <erickadikah2030@gmail.com>
2023-05-10 00:26:06 +0300

    raising ValueError

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -60,7 +60,7 @@ class DB:
             raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
-            raise NoResultFound
+            raise ValueError
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
