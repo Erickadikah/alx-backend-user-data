fd31dc741137a61d4734b51ccc4eca9077cca5af
erickadikah <erickadikah2030@gmail.com>
2023-05-10 23:55:44 +0300

    logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """flask_app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 AUTH = Auth()
 app = Flask(__name__)
@@ -45,12 +45,23 @@ def login() -> str:
     # if not valid_login:
     #     abort(400)
     if not AUTH.valid_login(email, password):
-        abort(401)
-
+       abort(401)
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie('session_id', session_id)
     return response
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> None:
+    """Logout route
+        destroy_session: user_id
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
