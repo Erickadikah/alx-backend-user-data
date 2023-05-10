8d6582ecc1e571e4d8d13e366c37ba43bd2cd873
erickadikah <erickadikah2030@gmail.com>
2023-05-10 14:55:22 +0300

    login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """flask_app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 AUTH = Auth()
 app = Flask(__name__)
@@ -33,16 +33,22 @@ def user() -> str:
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login():
+def login() -> str:
     """Login Route
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    try:
-        if not user:
-            return Flask.abort, 401
-    except Exception:
-        return
+
+    if not email or not password:
+        abort(400)
+    if not Auth.valid_login(email, password):
+        abort(401)
+
+    session_id = Auth.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 if __name__ == "__main__":
