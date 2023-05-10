c879d14d071d56cb819503e95994bb14d152116e
erickadikah <erickadikah2030@gmail.com>
2023-05-11 00:00:34 +0300

    logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,13 +52,13 @@ def login() -> str:
     return response
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> None:
+def logout():
     """Logout route
         destroy_session: user_id
     """
-    session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
+    cookies = request.cookies.get('session_id', None)
+    user = AUTH.get_user_from_session_id(cookies)
+    if user is None or user is None:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect('/')
