2430f3891b5138fc9011248dda5f27278a79a615
erickadikah <erickadikah2030@gmail.com>
2023-05-11 19:40:07 +0300

    Logout functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,7 +56,7 @@ def logout():
     """Logout route
         destroy_session: user_id
     """
-    cookies = request.cookie.get('session_id', None)
+    cookies = request.cookies.get('session_id', None)
     user = AUTH.get_user_from_session_id(cookies)
     if user is None or cookies is None:
         abort(403)
