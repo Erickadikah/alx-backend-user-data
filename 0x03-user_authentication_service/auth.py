739207c300f303dc43098f12f60445dadb9138f4
erickadikah <erickadikah2030@gmail.com>
2023-05-11 19:09:44 +0300

    delete session fix2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,7 +68,7 @@ class Auth:
         except Exception:
             return None
 
-    def destroy_session(self, user_id: int) -> None:
+    def destroy_session(self, user_id: str) -> None:
         """Destroy session
             Args: user_id
             find the user by user_id:
@@ -77,7 +77,7 @@ class Auth:
         """
         if user_id:
             try:
-                user = Auth._db.find_user_by(id=user_id)
+                user = self._db.find_user_by(id=user_id)
                 self._db.update_user(user.id, session=None)
                 # return user
             except NoResultFound:
