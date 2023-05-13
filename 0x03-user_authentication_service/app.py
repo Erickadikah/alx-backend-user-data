a08a542be5670d29465899cae2b1b0ba660082e5
erickadikah <erickadikah2030@gmail.com>
2023-05-13 12:03:40 +0300

    get profile correction

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,8 +79,8 @@ def logout() -> str:
 def profile() -> str:
     """Get profile route
     """
-    session_id = AUTH.get_user_from_session_id('session_id')
-    user = request.cookies.get(session_id)
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
     return jsonify({"email": user.email}), 200
