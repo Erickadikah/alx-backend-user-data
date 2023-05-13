d1685007caa58dc65a35d916619a2d08d76222b7
erickadikah <erickadikah2030@gmail.com>
2023-05-13 11:58:28 +0300

    redirect home

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,12 +67,11 @@ def logout() -> str:
             AUTH.destroy_session(user.id)
             return redirect(url_for('home'))
     """
-    session_id = request.form.get('session_id')
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if not user or not session_id:
+    if user is None:
         abort(403)
-    else:
-        AUTH.destroy_session(user.id)
+    AUTH.destroy_session(user.id)
     return redirect(url_for('/'))
 
 
